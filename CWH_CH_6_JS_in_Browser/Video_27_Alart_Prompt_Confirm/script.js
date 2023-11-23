c6c1520b8875b9bcaab566858f04618bb88caf6e
Yogesh1092 <yogeshkumar.fnd@gmail.com>
2023-11-23 09:45:39 +0530

    adding video27

---
+++ CWH_CH_6_JS_in_Browser/Video_27_Alart_Prompt_Confirm/script.js
@@ -0,0 +1,9 @@
+alert("hello this is alert provide the value of a");
+let a=prompt("enter the value of a" , "578"); //we can aslo provide a default value in prompt.
+
+let write=confirm("Do you want to write on page")
+if (write) {
+    document.write(a);
+}else{
+    document.write("Provide some text please");
+}
